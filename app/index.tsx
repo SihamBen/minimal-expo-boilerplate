89246e81c75ebc6d313fb4353a0d63d5a616b19d
Siham Bendaia <siham.bendaia37@gmail.com>
2023-05-25 19:00:16 +0100

    refactor: use react router

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { Text, View } from 'react-native';
+import { usePostActions } from '../store/postsStore';
+import FlashMessage from 'react-native-flash-message';
 
 export default function App() {
+  const { fetchPosts } = usePostActions();
+  console.log(fetchPosts());
   return (
     <View className="flex-1 bg-red-200 justify-center ">
+      <FlashMessage position="top" />
       <Text className="text-center">Basic Boilerplate</Text>
       <StatusBar style="auto" />
     </View>
